62f470b612d3d78c1f8c9b7311b4b1e29c4a0099
Hamdi Wahid <h.wahid160@gmail.com>
2020-01-30 15:44:15 +0100

    login2

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,32 @@ var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
 /* GET login. */
 router.get('/', function(req, res, next) {
+ 
   res.render('index', { title: 'Express' });
 });
 
+router.post('/sign-in', function(req, res, next) {
+        res.redirect('/homepage');
+    }
+);
+
+router.get('/homepage', function(req, res, next){
+
+    res.render('homepage', {});
+});
+
+router.post('/sign-up', function(req, res, next) {
+    res.redirect('/homepage');
+}
+);
+
+router.get('/homepage', function(req, res, next){
+
+res.render('homepage', {});
+});
+      
+ 
+   
 
 
 /* GET home page. */
